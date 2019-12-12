4f4ca3cb765e72dc2fccc090fbab689e98e76819
Wojciech Banaś <fizista@gmail.com>
2019-12-12 13:50:28 +0100

    umqtt.simple2: Init repository

--- setup.py
+++ setup.py
@@ -1,20 +1,42 @@
 import sys
+from pathlib import Path
+
+__dir__ = Path(__file__).absolute().parent
 # Remove current dir from sys.path, otherwise setuptools will peek up our
 # module instead of system's.
 sys.path.pop(0)
 from setuptools import setup
+
 sys.path.append("..")
 import sdist_upip
 
-setup(name='micropython-umqtt.simple',
-      version='1.3.4',
-      description='Lightweight MQTT client for MicroPython.',
-      long_description=open('README.rst').read(),
-      url='https://github.com/micropython/micropython-lib',
-      author='Paul Sokolovsky',
-      author_email='micro-python@googlegroups.com',
-      maintainer='micropython-lib Developers',
-      maintainer_email='micro-python@googlegroups.com',
-      license='MIT',
-      cmdclass={'sdist': sdist_upip.sdist},
-      packages=['umqtt'])
+
+def read(file_relative):
+    file = __dir__ / file_relative
+    with open(str(file)) as f:
+        return f.read()
+
+
+setup(
+    name='micropython-umqtt.simple2',
+    version='2.0.0',
+    description='Lightweight MQTT client for MicroPython.',
+    long_description=read('README.rst'),
+    long_description_content_type="text/x-rst",
+    url='https://github.com/fizista/micropython-umqtt.simple2',
+    author='Wojciech Banaś',
+    author_email='fizista@gmail.com',
+    maintainer='Wojciech Banaś',
+    maintainer_email='fizista+umqtt.simple2@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: Implementation :: MicroPython',
+    ],
+    keywords='mqtt micropython',
+    cmdclass={'sdist': sdist_upip.sdist},
+    packages=['umqtt'],
+    project_urls={
+        'Bug Reports': 'https://github.com/fizista/micropython-umqtt.simple2/issues',
+        'Source': 'https://github.com/fizista/micropython-umqtt.simple2',
+    },
+)
