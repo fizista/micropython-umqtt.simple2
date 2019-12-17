df6e8be32ffd054f1b154342cf13fc6d9a3916a5
Wojciech Banaś <fizista@gmail.com>
2019-12-17 12:06:47 +0100

    umqtt.simple2: Implementation of disable_net and enable_net

--- example_test_main.py
+++ example_test_main.py
@@ -3,16 +3,42 @@
 # ###
 import machine
 import utime
+import network
 from ubinascii import hexlify
 
 utime.sleep(2)
 
 import tests as tests_mod
 
+
+class TestMQTT(tests_mod.TestMQTT):
+    def disable_net(self):
+        # Works with esp32 and esp8266 ports.
+        # If it doesn't work with your port, please rewrite this part.
+        wlan = network.WLAN(network.STA_IF)
+        wlan.disconnect()
+        for i in range(500):
+            utime.sleep_ms(100)
+            if not wlan.isconnected():
+                return
+        raise Exception('Network disconnection problem!')
+
+    def enable_net(self):
+        # Works with esp32 and esp8266 ports.
+        # If it doesn't work with your port, please rewrite this part.
+        wlan = network.WLAN(network.STA_IF)
+        wlan.connect()
+        for i in range(500):
+            utime.sleep_ms(100)
+            if wlan.isconnected():
+                return
+        raise Exception('Network connection failure!')
+
+
 # 1883 : MQTT, unencrypted
 # 8883 : MQTT, encrypted
 # 8884 : MQTT, encrypted, client certificate required
-tests = tests_mod.TestMQTT(
+tests = TestMQTT(
     hexlify(machine.unique_id()).decode('ascii'),
     'test.mosquitto.org',
     port=1883
