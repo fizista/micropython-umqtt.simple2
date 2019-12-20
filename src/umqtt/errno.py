15359427f55ae6aa600a1a5f93674ab22dfa7b2b
Wojciech Banaś <fizista@gmail.com>
2019-12-20 13:49:24 +0100

    umqtt.simple2: Remove unused error codes

--- src/umqtt/errno.py
+++ src/umqtt/errno.py
@@ -4,8 +4,7 @@ EREADLEN = 2    # Wrong length of read data
 EWRITELEN = 3   # Wrong length of read data
 ESTRTOLONG = 4  # String too long !!!!
 ERESPONSE = 6   # Wrong response
-ELOCK = 10      # Currently, the MQTT client is blocked. It is probably waiting for an answer.
-                # The lock will be removed when the customer receives a response.
+
 ECONUNKNOWN = 20     # Connection refused, unknown error
 ECONPROTOCOL = 21    # Connection refused, unacteptable protocol version
 ECONREJECT = 22      # Connection refused, identifier rejected
@@ -15,8 +14,6 @@ ECONAUTH = 25        # Connection refused, not authorized
 
 ESUBACKUNKNOWN = 40  # Subscribe confirm unknown fail, SUBACK
 ESUBACKFAIL = 44     # Subscribe confirm response: Failure
-
-EUNIMPLEMENTED = 100
 
 # Status code numbers from set_callback_status()
 STIMEOUT = 0    # timeout
